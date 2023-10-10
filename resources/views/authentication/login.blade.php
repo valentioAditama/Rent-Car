265387b2b900bd5fd40880b3fcf41aa094181251
valentioaditama <valentioaditama8@gmail.com>
2023-10-10 15:40:56 +0700

    update

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -17,7 +17,7 @@
                 </div>
                 <div class="mb-3">
                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900">password</label>
-                    <input type="password" id="password" name="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email@gmail.com" required>
+                    <input type="password" id="password" name="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="password" required>
                 </div>
                 <div class="flex items-center mb-3">
                     <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">

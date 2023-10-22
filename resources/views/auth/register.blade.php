5625ef3213cb25e08181d7ad5a17fa6e225cc8e4
valentioaditama <valentioaditama8@gmail.com>
2023-10-22 13:49:32 +0700

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,7 +20,7 @@
                     <input type="email" id="email" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email@gmail.com" required>
                 </div>
                 <div class="mb-3">
-                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900">assword</label>
+                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
                     <input type="password" id="password" name="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="password" required>
                 </div>
                 <div class="mb-3">
